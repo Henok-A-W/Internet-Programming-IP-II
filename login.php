b38ec82d308f84eddc85d370887a1f7080a4145c
Yohanis Chimdesa <johnchimdesa50@gmail.com>
2024-11-26 00:59:43 +0300

    Updated login page

--- login.php
+++ login.php
@@ -39,7 +39,7 @@ if(isset($_POST['submit'])){
 <div class="form-container">
 
    <form action="" method="post" enctype="multipart/form-data">
-      <h3>login now</h3>
+      <h3>login now:</h3>
       <?php
       if(isset($message)){
          foreach($message as $message){
